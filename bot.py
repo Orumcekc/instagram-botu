7eee17b819014d564f2164fe72d5957dc7ed74f7
Orumcekc <m5342220444@gmail.com>
2026-04-07 08:08:26 +0300

    carousel denemesi

--- bot.py
+++ bot.py
@@ -197,7 +197,7 @@ def send_email(subject, body, image_paths):
 
 def main():
     print("Otonom Bot Yeni Mimari Başlatıldı...")
-    is_carousel = random.random() < 0.30
+    is_carousel = True
     content, topic = generate_content(is_carousel)
     
     if not content: return
